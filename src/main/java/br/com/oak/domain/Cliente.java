d4384e92baf7d140ffd013eeb0862cf3eea5876f
dadsilva <dadsilva@rd.com.br>
2022-03-17 19:40:38 -0300

    Criando classe cliente

--- src/main/java/br/com/oak/domain/Cliente.java
+++ src/main/java/br/com/oak/domain/Cliente.java
@@ -1,10 +1,101 @@
 package br.com.oak.domain;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "TB_CLIENTE")
 public class Cliente {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+    private Long id;
+	
+    @Column(name = "nm_cliente")
+    private String nome;
+    
+    @Column(name="nr_telefone")
+    private String telefone;
+    
+    @Column(name="ds_email")
+    private String email;
+    
+    @Column(name = "dt_nascimento")
+    private LocalDate dtNasc;
+    
+    @Column(name="ds_genero")
+    private String genero;
+    
+//	@JoinColumn(name = "id_pedido")
+//	@OneToMany
+//	private Pedido pedido;
+    
+    
+    public Cliente() {
+    	
+    
+    	
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getDtNasc() {
+		return dtNasc;
+	}
+
+	public void setDtNasc(LocalDate dtNasc) {
+		this.dtNasc = dtNasc;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+    
+    
+    
+    
 
 }
