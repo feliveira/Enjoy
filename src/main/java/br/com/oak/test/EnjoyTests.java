91d2ef4d2c75578974a4b01f34b99980517f6eb4
feliveira <feliveira8@gmail.com>
2022-03-19 22:09:32 -0300

    Ticket medio funcionando

--- src/main/java/br/com/oak/test/EnjoyTests.java
+++ src/main/java/br/com/oak/test/EnjoyTests.java
@@ -11,7 +11,7 @@ public class EnjoyTests {
 		
 		EntityManager em = Persistence.createEntityManagerFactory("onboarding").createEntityManager();
 		String[] phones = {"19996548523","11996858532", "11997858523","11996898523","11996858523","11996548553","19996548594","11996542694","11996542704","19996542705"};
-		String telefoneCliente = phones[(int)(Math.random()* 10 - 1 ) + 1];
+		String telefoneCliente = phones[(int)(Math.random() * 10 - 1 ) + 1];
 		
 		System.out.println("==================== Enjoy ===================="
 							+"\nBoas vindas ao nosso estabelecimento!"
@@ -27,9 +27,11 @@ public class EnjoyTests {
 		+ em.createQuery("SELECT p.dataPedido FROM Pedido p INNER JOIN Cliente c ON c.id = p.cliente WHERE c.telefone = :phone ORDER BY p.dataPedido DESC")
 		.setParameter("phone", idCliente.getTelefone()).setMaxResults(1).getSingleResult());
 		
-		System.out.println("Frequência de visitas: ");
+		System.out.println("Frequência de visitas: "); // Todo
 		
-		System.out.println("Ticket médio: ");
+		Double ticketMedio = em.createQuery("SELECT AVG(p.valorTotal) FROM Pedido p INNER JOIN Cliente c ON c.id = p.cliente WHERE c.telefone = :phone", Double.class)
+				.setParameter("phone", idCliente.getTelefone()).getSingleResult();
+		System.out.printf("Ticket médio: %.2f\n", ticketMedio );
 		
 		System.out.println("Bebidas e Estilos favoritos:\n[1] Cerveja Pilsen \n[2] Chopp Trigo \n[3] Vinho Tinto "); // Todo
 	}
